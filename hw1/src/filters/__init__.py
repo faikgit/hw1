f860ddbac1bdb363a319995c24803522a9cafe56
faikgit <faik.azizi@students.finki.ukim.mk>
2025-11-23 15:42:58 +0100

    Delete hw1/src/filters directory

--- hw1/src/filters/__init__.py
+++
@@ -1,5 +0,0 @@
-# src/filters/__init__.py
-# Expose filter functions for simple imports
-from .filter1_symbols import run_filter1_symbols
-from .filter2_last_date import get_symbols_with_last_date
-from .filter3_missing_data import run_filter3_fill_missing
